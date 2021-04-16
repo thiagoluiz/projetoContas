b76598130b415d59561b6d22c0286b237f9d9b79
thiagoluiz <thiagoluiz.work@gmail.com>
2021-04-15 21:42:18 -0300

    Desenvolvimento dos componentes e chamadas para a API

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ProjetoContas
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.5.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.7.
 
 ## Development server
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'http://localhost:8083/api'
 };
 
 /*
@@ -14,3 +15,6 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+
+
